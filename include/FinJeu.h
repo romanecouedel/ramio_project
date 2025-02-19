c91d523c438473467c9cb40029e1368ff38771eb
Romane Couedel <romanecouedel@github.com>
2025-02-19 08:47:24 +0100

    Add FinDeJeu.h fonctionnel

---
+++ include/FinJeu.h
@@ -0,0 +1,31 @@
+
+#ifndef FINJEU_H
+#define FINJEU_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class FinDeJeu {
+public:
+    FinDeJeu(float windowWidth, float windowHeight);
+    void afficher(sf::RenderWindow& window, float tempsEcoule, int score);
+    bool handleInput(sf::Event event);
+
+private:
+    sf::Font font;
+    sf::Text titre;
+    sf::Text texteTemps;
+    sf::Text texteScore;
+    sf::Text texteInstructions;
+    sf::RectangleShape background;
+    sf::Texture backgroundTexture; // Ajout de la texture pour le fond d'écran
+    sf::Sprite backgroundSprite;   // Ajout du sprite pour le fond d'écran
+
+
+    sf::RectangleShape bouton; // Ajout du bouton
+    sf::Text boutonTexte;      // Texte du bouton
+
+    void setupText(sf::Text& text, const std::string& content, float x, float y, unsigned int size);
+};
+
+#endif 
